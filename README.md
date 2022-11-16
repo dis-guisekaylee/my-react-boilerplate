d8c04c9a37417f325a6f8af8962f0f670b2f10f9
Kaylee Guise <guisekayl@gmail.com>
2022-11-15 22:17:34 -0600

    Final push; project non-functioning at this time

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 This is my react-boilerplate
 
+It doesn't work! Most dependencies are out of date and I gave up on trying to tweak them all so they'd cooperate. :)
+
 ## Setup
 
 npm install
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,12 @@
-let path = require("path");
+const path = require("path");
+let nodeExternals = require("webpack-node-externals");
+let HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const moduleObj = {
+  loaders: [
+    { test: /\.js$/, exclude: /node_modules/, loaders: ["babel-loader"] },
+  ],
+};
 
 const client = {
   entry: { client: "./src/client/index.js" },
@@ -6,13 +14,26 @@ const client = {
   output: {
     filename: "[name].js",
     path: path.resolve(__dirname, "dist/public"),
+    module: moduleObj,
+    plugins: [new HtmlWebPackPlugin({ template: "src/client/index.html" })],
   },
 };
 
 const server = {
   entry: { server: "./src/server/index.js" },
   target: "node",
-  output: { filename: "[name].js", path: path.resolve(__dirname, "dist") },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+    module: moduleObj,
+    externals: [nodeExternals()],
+  },
 };
 
-module.exports = [client, server];
+module.exports = {
+  node: {
+    Buffer: false,
+    process: false,
+  },
+};
+// module.exports = [client, server];
